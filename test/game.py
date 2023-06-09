9a01015718581ef4270110d30843220a7258b2cf
YUK1 <116713322+YUK1-0000@users.noreply.github.com>
2023-06-09 08:48:54 +0000

    "Create class: Cloudable"

--- test/game.py
+++ test/game.py
@@ -1,12 +1,31 @@
 import pyxel
 
-from ..pyxel_pacl import Client
+from ..pyxel_pacl import Pacl
 
 
-c = Client()
+c = Pacl()
+c.start()
 
 
-class App:
+class Cloudable:
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        cls._cloudable_original = cls.update
+        cls.update = cls._update_override
+
+    def _update_override(self):
+        old_values = dict()
+        updated_values = dict()
+         self._cloudable_original(self)
+        for name in dir(self):
+            value = getattr(self, name):
+            if not callable(value):
+                if name in old_values and old_values[name] != value:
+                    updated_values[name] = value
+                old_values[name] = value
+
+
+class App(Cloudable):
     def __init__(self):
         pyxel.init(160, 120, title="Hello Pyxel")
         pyxel.image(0).load(0, 0, "assets/pyxel_logo_38x16.png")
@@ -22,4 +41,5 @@ class App:
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 
 
-App()
+app = App()
+app.update()
